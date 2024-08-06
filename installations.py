b28b25ce54a8a7844577bc5e4b738627458edc64
thearhansheth <arhan.sheth@gmail.com>
2024-08-06 15:16:24 +0530

    Installations file

---
+++ installations.py
@@ -0,0 +1,9 @@
+# Only run once before running other files to import all 
+# necessary libraries/frameworks
+import os
+
+# Install PyPDF2 to extract text from PDF files
+os.system('pip install PyPDF2')
+
+# Install pandas to handle CSV creation and manipulation (optional but useful)
+os.system('pip install pandas')
