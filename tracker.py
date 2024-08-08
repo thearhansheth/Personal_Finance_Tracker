0a3a0e7380be61a45fae8b0d0d975245a19c3a2d
thearhansheth <arhan.sheth@gmail.com>
2024-08-08 13:52:20 +0530

    Added json file storage to track yearly expenses

--- tracker.py
+++ tracker.py
@@ -10,21 +10,35 @@ def monthly_expenses(csv_file_path):
         next(reader)
         for row in reader:
             date, description, amount, balance = row
-            if amount < 0:
+            if float(amount) < 0.0:
                 total_expenses += abs(float(amount))
 
-    return total_expenses
+    return [date[0:2], total_expenses]
 
+def update_expenses(json_path, total_expenses):
+    expense_data = {}
+    if os.path.exists(json_path):
+        with open(json_path, 'r') as json_file:
+            expense_data = json.load(json_file)
+            month = int(total_expenses[0])
+    expense_data[month] = total_expenses[1]
+    with open(json_path, 'w') as json_file:
+        json.dump(expense_data, json_file, indent = 4)
+        
 
-print("Is your statement in CSV or PDF format?")
-question = input("Click 1 for CSV and 2 for PDF")
-if question == 1:
-    pdf_file = input("Input path of statement pdf file: ")
-    transactions = pdf2csv_converter.extract_transactions_from_pdf(pdf_file)
-    csv_path = pdf2csv_converter.write_to_csv(transactions, pdf_file)
-    monthly_expenses(csv_path)
-elif question == 2:
-    csv_file = input("Input path of statement csv file: ")
-    monthly_expenses(csv_file)
-else: 
-    print("Wrong Input, Try Again.")
+if __name__ == '__main__':
+    json_file_path = "/Users/arhan.sheth/Documents/Codes/Projects/Personal_Finance_Tracker/Yearly_Expenses/2024.json"
+    question = input("Is your statement file in PDF or CSV format? : ")
+    if question.lower() == 'pdf':
+        pdf_file = input("Input path of statement pdf file: ")
+        transactions = pdf2csv_converter.extract_transactions_from_pdf(pdf_file)
+        csv_path = pdf2csv_converter.write_to_csv(transactions, pdf_file)
+        expense = monthly_expenses(csv_path)
+        print(expense)
+        update_expenses(json_file_path, expense)
+    elif question.lower() == 'csv':
+        csv_file = input("Input path of statement csv file: ")
+        expense = monthly_expenses(csv_file)
+        update_expenses(json_file_path, expense)
+    else: 
+        print("Wrong Input, Try Again.")
