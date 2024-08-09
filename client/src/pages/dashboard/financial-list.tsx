79cb6603229969196544cd9baba586e524fc0eb3
thearhansheth <arhan.sheth@gmail.com>
2024-08-09 13:44:01 +0530

    Created basic skeleton of user dashboard and transaction form

---
+++ client/src/pages/dashboard/financial-list.tsx
@@ -0,0 +1,3 @@
+export const FinancialList = () => {
+    return <div>Record List</div>
+}
--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,5 +1,14 @@
+import { useUser } from '@clerk/clerk-react'
+import { FinancialRecord } from './financial-form';
+import { FinancialList } from './financial-list';
+
 export const Dashboard = () =>{
+    const { user } = useUser();
     return(
-        <h1>Dashboard</h1>
+        <div className="dashboard-container">
+            <h1>Welcome {user?.firstName}</h1>
+            <FinancialRecord/>
+            <FinancialList/>
+        </div>
     )
 }
